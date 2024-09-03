94c8cc5a50b771b7fbb0bf6d1f9cd32fabfd8b52
Hkhang096 <2051050190khang@ou.edu.vn>
2024-09-04 00:18:01 +0700

    create products detail

---
+++ WebBanSach/WebBanSach/Controllers/BookController.cs
@@ -0,0 +1,51 @@
+﻿using WebBanSach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLiBanSach02.Controllers
+{
+    public class BookController : Controller
+    {
+        private BookStoreEntities da = new BookStoreEntities();
+
+        // GET: BooK
+        public ActionResult Detail(int id)
+        {
+            Session["productID"] = id;
+            Product pro = da.Products.Where(s => s.ProductID == id).FirstOrDefault();
+            ViewBag.Products = da.Products.Where(p => p.CategoryID == pro.CategoryID).ToList();
+
+            return View(pro);
+        }
+
+        public ActionResult ViewComments(int id)
+        {
+            List<Comment> comments = da.Comments.Where(c => c.ProductId == id).ToList();
+
+            return View(comments);
+        }
+
+        public ActionResult AddComment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(Comment comment, FormCollection collection)
+        {
+            Comment c = new Comment();
+            c = comment;
+            c.ProductId = (int)Session["productID"];
+            c.UserId = (int)Session["UserID"];
+            c.CreatedAt = DateTime.Now;
+            da.Comments.Add(c);
+            da.SaveChanges();
+            return View();
+
+            //return Redirect("~/Book/Detail/" + (int)Session["productID"]);
+        }
+    }
+}
