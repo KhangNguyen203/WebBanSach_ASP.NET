e0833bcc3ba0156a9694ceea79e140e41e88c249
ldtThao <ldtt.thao@gmail.com>
2024-09-10 00:58:19 +0700

    create order and order detail.

--- WebBanSach/WebBanSach/Models/Model1.Designer.cs
+++ WebBanSach/WebBanSach/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\WebBanSach\WebBanSach_ASP.NET\WebBanSach\WebBanSach\Models\WebBanSach.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\SETUP\Desktop\New folder\WebBanSach_ASP.NET\WebBanSach\WebBanSach\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
