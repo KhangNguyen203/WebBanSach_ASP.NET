ee9295c4d26b97273c77cba5d33ef54f0b1b11ff
KhangNguyen203 <kaypi096@gmail.com>
2024-09-11 13:55:36 +0700

    create thong ke

--- WebBanSach/WebBanSach/Areas/Admin/Controllers/DashBoardController.cs
+++ WebBanSach/WebBanSach/Areas/Admin/Controllers/DashBoardController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebBanSach.Models;
 
-namespace QuanLiBanSach02.Areas.Admin.Controllers
+namespace WebBanSach.Areas.Admin.Controllers
 {
     public class DashBoardController : Controller
     {
@@ -23,12 +23,12 @@ namespace QuanLiBanSach02.Areas.Admin.Controllers
             return View();
         }
 
-        //public ActionResult StatsByMonth()
-        //{
-        //    var lsDataStatsByMonth = da.ThongKeDoanhThuTheoThang();
+        public ActionResult StatsByMonth()
+        {
+            var lsDataStatsByMonth = da.ThongKeDoanhThuTheoThang();
 
-        //    return Json(lsDataStatsByMonth, JsonRequestBehavior.AllowGet);
-        //}
+            return Json(lsDataStatsByMonth, JsonRequestBehavior.AllowGet);
+        }
 
         //public ActionResult ThongKeSoLuongSachTheoTheLoai()
         //{
