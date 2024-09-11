4336030391f3168ca224e0876d3458588939a028
KhangNguyen203 <kaypi096@gmail.com>
2024-09-11 12:58:45 +0700

    create product manage

--- WebBanSach/WebBanSach/Controllers/HomeController.cs
+++ WebBanSach/WebBanSach/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
-namespace QuanLiBanSach02.Controllers
+namespace WebBanSach.Controllers
 {
     public class HomeController : Controller
     {
